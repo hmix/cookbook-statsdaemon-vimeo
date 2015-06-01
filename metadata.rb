241903f9a48ff22a6f18f5a47b9529482b69c840
Michael Burns <michael.burns@rackspace.com>
2015-06-01 02:15:48 -0700

    update maintainer and bump version

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,13 @@
 name             'statsdaemon-vimeo'
-maintainer       'Olivier Dolbeau'
-maintainer_email 'odolbeau@gmail.com'
+maintainer       'Michael Burns'
+maintainer_email 'michael@mirwin.net'
 license          'MIT'
 description      'Installs statsdaemon (vimeo)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.2'
+version          '1.0.3'
+
+source_url 'https://github.com/mburns/cookbook-statsdaemon-vimeo' if respond_to?(:source_url)
+issues_url 'https://github.com/mburns/cookbook-statsdaemon-vimeo/issues' if respond_to?(:issues_url)
 
 recipe 'default', 'Installs statsdaemon.'
 
